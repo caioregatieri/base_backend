b04a95fc9fd8b40481cb2e2bd7bef7e0b3340d22
Caio Regatieri <caio_fr_sp@hotmail.com>
2020-04-10 15:53:09 -0300

    Load dinamic route files

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
 
-router.use('/auth',   require('./auth'));
-
-router.use('/testes', require('./testes'));
-
-router.use('/users',  require('./users'));
+fs.readdirSync(__dirname)
+    .filter(file => file.split('.')[1] == 'js' && file != 'index.js')
+    .forEach(file => {
+        const f = file.split('.')[0];
+        router.use(`/${f}`, require(`./${f}`))
+    });
 
 module.exports = router;
